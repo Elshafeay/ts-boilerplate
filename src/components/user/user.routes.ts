a6dbcf681062eb54af6e9778e22efbacd7123b41
elshafeay-trufla <mohamedelshafeay@gmail.com>
2022-05-10 22:15:35 +0200

    Added signup schema validation

--- src/components/user/user.routes.ts
+++ src/components/user/user.routes.ts
@@ -2,12 +2,12 @@ import express from 'express';
 import { validateRequest } from '../../middlewares/validate-request';
 
 import UserController from './user.controller';
-import { getUserValidation } from './user.schemas';
+import { createUserValidation, getUserValidation } from './user.schemas';
 
 const userRouter = express.Router();
 
 userRouter.get('/users', UserController.getUsers);
 userRouter.get('/users/:id', validateRequest(getUserValidation), UserController.getUser);
-userRouter.post('/users', UserController.signUp);
+userRouter.post('/users', validateRequest(createUserValidation), UserController.signUp);
 
 export default userRouter;
